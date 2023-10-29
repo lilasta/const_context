dcc75649c638dbef4ce23bacf1ac92a6d6bb932f
lilasta <lilaceous@proton.me>
2023-10-29 15:26:51 +0900

    example/lock: RcLock

--- examples/lock.rs
+++ examples/lock.rs
@@ -1,4 +1,5 @@
-use core::cell::UnsafeCell;
+use std::cell::UnsafeCell;
+use std::rc::Rc;
 
 use const_context::action::Action;
 use const_context::{ctx, ctx_if};
@@ -29,7 +30,7 @@ impl<const ID: Id, T> Lock<ID, T> {
         }
     }
 
-    pub fn modify<'a>(&'a self, f: impl FnOnce(&'a mut T) + 'a) -> impl 'a + Action {
+    pub fn modify<'a>(&'a self, f: impl FnOnce(&mut T) + 'a) -> impl 'a + Action {
         ctx_if! {
             if set (Locked<ID>, ()) then
                 ctx! { let _ = f(unsafe { &mut *UnsafeCell::raw_get(&self.0) }); }
@@ -39,9 +40,42 @@ impl<const ID: Id, T> Lock<ID, T> {
     }
 }
 
+pub struct RcLock<const ID: Id, T>(Rc<UnsafeCell<T>>);
+
+impl<const ID: Id, T> RcLock<ID, T> {
+    pub fn new(value: T) -> Self {
+        Self(Rc::new(UnsafeCell::new(value)))
+    }
+
+    pub fn lock(&self) -> impl Action {
+        ctx_if! {
+            if set (Locked<ID>, ()) then
+                ctx! { panic "Double locks" }
+            else
+                ctx! { set Locked<ID>: () = () where const ID: Id = ID; }
+        }
+    }
+
+    pub fn unlock(&self) -> impl Action {
+        ctx! {
+            unset (Locked<ID>, ());
+        }
+    }
+
+    pub fn modify(&self, f: impl FnOnce(&mut T)) -> impl Action {
+        let inner = self.0.clone();
+
+        ctx_if! {
+            if set (Locked<ID>, ()) then
+                ctx! { let _ = f(unsafe { &mut *UnsafeCell::raw_get(&*inner) }); }
+            else
+                ctx! { panic "Not locked" }
+        }
+    }
+}
+
 fn main() {
     let ref lock = Lock::<1, _>::new(1);
-
     let action = ctx! {
         lock.lock();
         lock.modify(|v| *v += 1);
@@ -49,6 +83,15 @@ fn main() {
         lock.modify(|v| println!("{}", *v));
         lock.unlock();
     };
+    action.start_eval();
 
+    let action = ctx! {
+        let rclock = RcLock::<1, _>::new(1);
+        rclock.lock();
+        rclock.modify(|v| *v += 1);
+        rclock.modify(|v| *v *= 21);
+        rclock.modify(|v| println!("{}", *v));
+        rclock.unlock();
+    };
     action.start_eval();
 }
